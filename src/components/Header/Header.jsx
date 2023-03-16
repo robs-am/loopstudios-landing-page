4e0a8f4f2d2a6b1c299c6b2a8e82fb7793204187
Roberta Amaro <roberta.amaro89@gmail.com>
2023-03-16 20:31:24 -0300

    add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Navbar from "./NavBar/Navbar";
+import logo from "../../assets/svg/logo.svg";
 
 const Header = () => {
-  return <Navbar />;
+  return (
+    <>
+      <img src={logo} alt="logo loopstudios" />
+      <Navbar />
+    </>
+  );
 };
 
 export default Header;
