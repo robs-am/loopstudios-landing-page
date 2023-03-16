89601bb865293f39a5a2021a51edc39f61c86653
Roberta Amaro <roberta.amaro89@gmail.com>
2023-03-16 20:43:47 -0300

    altera posição dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <Banner />
       <Header />
-      <h1>Hello</h1>
+      <Banner />
     </div>
   );
 }
